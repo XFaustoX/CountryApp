c68eae277c13b5f1a641ff28692a7612790ba4eb
XFaustoX <pedro.borroni.m@gmail.com>
2024-10-31 20:41:14 -0300

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //path para que en la url diga home
+    path: '',
+    // component la pagina que se mostrara
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: '**',
+    //para redirigir a la pagina de home
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
